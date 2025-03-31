39e7b6054cf166e6f00a73d0b17394ee4d7377be
BenComer98 <benalancomer@gmail.com>
2025-03-31 12:12:26 -0700

    Trying Program.cs fix

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BenComerSoftware;
+using Microsoft.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var navigationManager = builder.Services.BuildServiceProvider().GetRequiredService<NavigationManager>();
 
 await builder.Build().RunAsync();
